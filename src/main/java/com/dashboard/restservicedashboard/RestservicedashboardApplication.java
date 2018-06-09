72d82393e64a3824b381d5d0f1b257dc3e5d2bbe
U-EMEA\pinzi <pinzi@WHCHM45BCL.emea.hpqcorp.net>
2018-06-09 15:09:41 +0200

    Init Springboot

---
+++ src/main/java/com/dashboard/restservicedashboard/RestservicedashboardApplication.java
@@ -0,0 +1,12 @@
+package com.dashboard.restservicedashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestservicedashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestservicedashboardApplication.class, args);
+	}
+}
